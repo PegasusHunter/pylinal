d6464b17ff03c47ebbc7472fdb82b093cb603692
cospectrum <severinalexeyv@gmail.com>
2022-03-20 23:33:31 +0300

    Add vector and matrix functions

---
+++ tests/test_matrixfunc.py
@@ -0,0 +1,43 @@
+import random
+from pylinal import Matrix
+
+from pylinal import MatrixFunc
+
+
+class TestMatrixFunc:
+
+    @staticmethod
+    def run_tests():
+        tests = [f for f in dir(TestMatrixFunc) if f.startswith('test_')]
+        for method in tests:
+            eval(f'TestMatrixFunc.{method}()')
+
+    @staticmethod
+    def test_example():
+        rand = lambda: random.randint(-10, 10)
+
+        shape = (abs(rand()), abs(rand()))
+        rows = [
+            [rand() for _ in range(shape[1])]
+            for _ in range(shape[0])
+        ]
+        matrix = Matrix(rows)
+
+        f = MatrixFunc(rows)
+        assert f() == matrix
+
+        scalar = rand()
+        assert scalar * f() == (scalar * f)() == scalar * matrix
+        assert f() * scalar == (f * scalar)() == matrix * scalar
+
+        assert f() + matrix == (f + matrix)() == 2*matrix
+        assert f().T == f.T() == matrix.T
+
+
+def main():
+    TestMatrixFunc.run_tests()
+    print('success!')
+
+
+if __name__ == '__main__':
+    main()
