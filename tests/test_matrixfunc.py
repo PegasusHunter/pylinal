8fbb55f4a8cc411ee5e96e257c92791365134b92
cospectrum <severinalexeyv@gmail.com>
2022-04-19 13:14:01 +0300

    Add flatten method

--- tests/test_matrixfunc.py
+++ tests/test_matrixfunc.py
@@ -1,10 +1,52 @@
 import random
-from pylinal import Matrix
+import itertools
+from typing import List, Tuple
 
-from pylinal import MatrixFunc
+from pylinal import Matrix, MatrixFunc, VectorFunc
+
+
+def random_rows(shape: Tuple[int, int], integer: bool = False) -> List:
+    dim_0, dim_1 = shape
+    prng = lambda: random.random() - 1/2
+
+    if integer:
+        prng = lambda: random.randint(-10, 10)
+
+    rows: List[List[float]] = [
+        [prng() for _ in range(dim_1)]
+        for _ in range(dim_0)
+    ]
+    return rows
 
 
 class TestMatrixFunc:
+
+    def test_flatten(self):
+        rows = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+        m = MatrixFunc(rows)
+        
+        def chain(rows: List[List]) -> List:
+            flatten = [el for row in rows for el in row]
+            return flatten
+
+        assert m.flatten()() == VectorFunc(el for el in itertools.chain(*rows))()
+        assert m.flatten()() == VectorFunc(rows[0] + rows[1] + rows[2])()
+
+        tries = random.randint(5, 10)
+        for _ in range(tries):
+            shape = (random.randint(1, 10), random.randint(1, 10))
+            rows = random_rows(shape, integer=True)
+            
+            m = MatrixFunc(rows)
+            
+            assert m.flatten()() == VectorFunc(chain(rows))()
+            assert m.flatten()() == VectorFunc(itertools.chain(*rows))()
+
+        return
 
     @staticmethod
     def run_tests():
